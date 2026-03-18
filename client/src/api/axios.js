da0badeb7a5dbcae6ff223a1d4bd4d4ac3d6ee98
aquaminti <lutsemer@mail.ru>
2026-03-18 17:31:43 +0500

    Change axios base URL to production endpoint

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://goal-tracker-98zg.onrender.com/api',
 });
 
 instance.interceptors.request.use((config) => {
